5c728c71cdcf6c00ad13056cec0a35d3d4ba3eb4
Daní Pinart <dpinart@uxland.cat>
2024-06-09 11:06:47 +0200

    chore: Demo setup shell script added

---
+++ cerverus-setup.ps1
@@ -0,0 +1,31 @@
+$repoUrl = "https://git.jetbrains.space/papanell/cerverus/cerverus-backoffice.git"
+$branch  = "demo"
+$folderName = (Split-Path $repoUrl -Leaf).Replace(".git", "")
+$snapshotsFolder = "Snapshots"
+
+if(Test-Path $folderName) {
+    # Navigate into the directory
+    Set-Location -Path $folderName
+
+    # Checkout the desired branch
+    git checkout $branch
+
+    # Pull the latest changes
+    git pull
+} else {
+    # Clone the repository if the directory does not exist
+    git clone -b $branch $repoUrl
+
+    # Navigate into the directory
+    Set-Location -Path $folderName
+}
+# Check if the Snapshots directory exists
+if(-not (Test-Path $snapshotsFolder))
+{
+    # Create the Snapshots directory
+    New-Item -ItemType Directory -Path $snapshotsFolder
+}
+
+docker-compose down
+
+docker-compose -f docker-compose.demo.yml up -d --build
