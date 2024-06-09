0dbf77947c985bdd8ec5469ec7fcae2abd39c60a
Daní Pinart <dpinart@uxland.cat>
2024-06-09 10:57:02 +0200

    feat: fix Docker deployment

--- src/core/image-processing/PythonImageProcessor/Bootstrapper.cs
+++ src/core/image-processing/PythonImageProcessor/Bootstrapper.cs
@@ -1,4 +1,5 @@
-﻿using Cerverus.Maintenance.Features.Features.Analysis.AnalyzeCapture;
+﻿using System.Runtime.InteropServices;
+using Cerverus.Maintenance.Features.Features.Analysis.AnalyzeCapture;
 using Microsoft.Extensions.DependencyInjection;
 using Python.Runtime;
 
@@ -15,14 +16,44 @@ public static class Bootstrapper
     
     private static IServiceCollection InitPythonEngine(this IServiceCollection services)
     {
-        //Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", "/opt/homebrew/bin/python3");
-        if(Environment.GetEnvironmentVariable("PYTHONNET_PYDLL") == null)
-            Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", @"C:\Python312\python312.dll");
-        //Environment.SetEnvironmentVariable("PYTHONPATH", @"C:\Python312\DLL");
-        //PythonEngine.PythonHome =  @"C:\Python312";
-       // PythonEngine.PythonPath = @"C:\Python312;C:\Python312\DLLs;C:\Python312\Lib;C:\Python312\Lib\site-packages";
+        SetEnvironmentVariables();
         PythonEngine.Initialize();
-       PythonEngine.BeginAllowThreads();
+        PythonEngine.BeginAllowThreads();
         return services;
+    }
+    
+    private static void SetEnvironmentVariables()
+    {
+        if(!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PYTHONNET_PYDLL")))
+            return;
+        if(File.Exists("/tmp/python_version"))
+            SetEnvironmentVariablesFromFile();
+        else 
+            SetEnvironmentVariablesFromPlatform();
+    }
+
+    private static void SetEnvironmentVariablesFromPlatform()
+    {
+        var pythonDllPath = string.Empty;
+        if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            pythonDllPath = @"C:\Python312\python312.dll";
+        else if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            pythonDllPath = @"C:\Python312\python312.dll";
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            pythonDllPath = "/opt/homebrew/bin/python3";
+        Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", pythonDllPath, EnvironmentVariableTarget.Process);
+    }
+    private static void SetEnvironmentVariablesFromFile()
+    {
+        var pythonVersion = File.ReadAllText("/tmp/python_version").Trim();
+        var architecture = RuntimeInformation.ProcessArchitecture == Architecture.X64 ? "x86_64" : "aarch64";
+        var pythonNetDll = $"/usr/lib/{architecture}-linux-gnu/libpython{pythonVersion}.so";
+        Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", pythonNetDll, EnvironmentVariableTarget.Process);
+        var currentPath = Environment.GetEnvironmentVariable("PATH");
+        //add ffmpeg path to PATH
+        currentPath += $"{Path.PathSeparator}/user/bin/ffmpeg";
+        //add python lib path to PATH
+        currentPath += $"{Path.PathSeparator}/usr/local/bin/python{pythonVersion}{Path.PathSeparator}/usr/bin{Path.PathSeparator}/usr/local/bin";
+        Environment.SetEnvironmentVariable("PATH", currentPath, EnvironmentVariableTarget.Process);
     }
 }
