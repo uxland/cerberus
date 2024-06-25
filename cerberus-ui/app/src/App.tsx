594944954ded449b5b53cf561822c6c31901ae30
Ignasi Reixach <ignasireixach@gmail.com>
2024-06-25 13:38:56 +0200

    task: config mui theme and tailwind

--- cerberus-ui/app/src/App.tsx
+++ cerberus-ui/app/src/App.tsx
@@ -1,16 +1,18 @@
-import './App.css'
-import { dummy } from '@cerberus/core';
-import { MaintenanceApp } from '@cerberus/maintenance';
-import { OrganizationalStructureApp } from '@cerberus/organizational-structure';
+import {dummy} from '@cerberus/core';
+import {MaintenanceApp} from '@cerberus/maintenance';
+import {OrganizationalStructureApp} from '@cerberus/organizational-structure';
+import {Typography} from '@mui/material';
+import './styles/App.css';
 function App() {
   dummy();
   return (
     <>
+      <Typography variant='h1'>APP</Typography>
       <h2>APP</h2>
       <MaintenanceApp />
       <OrganizationalStructureApp />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ cerberus-ui/app/src/styles/mui/theme.ts
@@ -0,0 +1,67 @@
+import blueGrey from '@mui/material/colors/blueGrey';
+import createTheme from '@mui/material/styles/createTheme';
+import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfigModule from '../../../tailwind.config';
+
+const tailwindConfig = resolveConfig(tailwindConfigModule);
+
+const paletteTheme = createTheme({
+  palette: {
+    primary: {
+      main: `${tailwindConfig.theme.colors.primary}`,
+    },
+  },
+});
+
+const theme = createTheme(paletteTheme, {
+  typography: {
+    htmlFontSize: 14,
+    fontFamily: 'Roboto',
+    h1: {
+      fontSize: createTheme().typography.pxToRem(18),
+      textTransform: 'uppercase',
+      letterSpacing: '1px',
+      fontWeight: '900',
+      color: `${tailwindConfig.theme.colors.primary}`,
+    },
+    h2: {
+      fontSize: createTheme().typography.pxToRem(16),
+      textTransform: 'uppercase',
+      letterSpacing: '1px',
+      fontWeight: '500',
+      color: 'black',
+    },
+    h3: {
+      fontSize: createTheme().typography.pxToRem(14),
+      textTransform: 'uppercase',
+      letterSpacing: '1px',
+      fontWeight: '500',
+      color: 'black',
+    },
+    h4: {
+      fontSize: createTheme().typography.pxToRem(14),
+      color: blueGrey[900],
+      textTransform: 'uppercase',
+      letterSpacing: '.5px',
+      fontWeight: '500',
+    },
+    h5: {
+      fontSize: createTheme().typography.pxToRem(12),
+      color: blueGrey[900],
+      textTransform: 'uppercase',
+      letterSpacing: '.5px',
+      fontWeight: '500',
+    },
+    subtitle1: {
+      fontSize: createTheme().typography.pxToRem(16),
+      fontWeight: '500',
+      lineHeight: '21px',
+      color: blueGrey[900],
+    },
+    body1: {
+      fontSize: createTheme().typography.pxToRem(14),
+    },
+  },
+});
+
+export default theme;
