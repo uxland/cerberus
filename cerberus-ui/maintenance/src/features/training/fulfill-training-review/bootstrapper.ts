71716b82239515b0858c722c9e5f891f2c3b94d5
Ignasi Reixach <ignasireixach@gmail.com>
2024-07-05 14:54:02 +0200

    feat: training form

--- cerberus-ui/maintenance/src/features/training/fulfill-training-review/bootstrapper.ts
+++ cerberus-ui/maintenance/src/features/training/fulfill-training-review/bootstrapper.ts
@@ -1,12 +1,13 @@
 import {addRoute, registerRouteComponent, store} from '@cerberus/core';
 import {Container} from 'inversify';
 import {requestHandler} from 'mediatr-ts';
-import {FulfillTrainingReview, CommandHandler} from './command.ts';
+import {CommandHandler, FulfillTrainingReview} from './command.ts';
 import {FulfillTrainingReviewPage} from './component.tsx';
 import {
   GetPendingTrainingReview,
   QueryHandler,
-} from './getPendingTrainingReview.ts';
+} from './get-pending-training-review.ts';
+
 export const bootstrapFulfillTrainingReview = (container: Container) => {
   registerRouteComponent(
     FulfillTrainingReviewPage.name,
--- cerberus-ui/maintenance/src/features/training/fulfill-training-review/component.tsx
+++ cerberus-ui/maintenance/src/features/training/fulfill-training-review/component.tsx
@@ -1,25 +1,26 @@
 import {getImageUrl, nop} from '@cerberus/core';
-import {
-  Divider,
-  FormControlLabel,
-  FormGroup,
-  Paper, TextareaAutosize,
-  ToggleButton,
-  Typography,
-} from '@mui/material';
+import {Box, FormControl} from '@mui/material';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import Paper from '@mui/material/Paper';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import ToggleButton from '@mui/material/ToggleButton';
+import Typography from '@mui/material/Typography';
 import {Mediator} from 'mediatr-ts';
 import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import {FilterResult} from '../../issues/show-issue/model.ts';
-import {GetPendingTrainingReview} from './getPendingTrainingReview.ts';
+import {FulfillTrainingReview} from './command.ts';
+import {GetPendingTrainingReview} from './get-pending-training-review.ts';
 import {
   FilterResultReview,
+  TrainingReview,
   initialFilterResultReview,
   isValidReview,
-  TrainingReview, TrainingReviewResult,
-  updateTrainingReviewResult
+  updateTrainingReviewResult,
 } from './model.ts';
-import {FulfillTrainingReview} from "./command.ts";
 
 export const FulfillTrainingReviewPage = () => {
   const {id} = useParams();
@@ -59,7 +60,9 @@ export const FulfillTrainingReviewPage = () => {
 };
 
 const FiltersReview = (props: {trainingReview: TrainingReview}) => {
-  const [reviewResult, setReviewResult] = useState(initialFilterResultReview(props.trainingReview));
+  const [reviewResult, setReviewResult] = useState(
+    initialFilterResultReview(props.trainingReview)
+  );
   const [canSend, setCanSend] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [error, setError] = useState<string>(undefined);
@@ -72,15 +75,14 @@ const FiltersReview = (props: {trainingReview: TrainingReview}) => {
     try {
       setError(undefined);
       setIsSubmitting(true);
-      await new Mediator().send(new FulfillTrainingReview(props.trainingReview.id, reviewResult));
-    }
-    catch (e){
+      await new Mediator().send(
+        new FulfillTrainingReview(props.trainingReview.id, reviewResult)
+      );
+    } catch (e) {
       setError(e.message || e.toString);
+    } finally {
+      setIsSubmitting(false);
     }
-    finally {
-      setIsSubmitting(false)
-    }
-
   };
 
   return (
@@ -98,26 +100,50 @@ const FiltersReview = (props: {trainingReview: TrainingReview}) => {
             <Typography className='!text-lg'>Review results</Typography>
             <Divider orientation='horizontal' className='bg-gray-300 !h-0' />
           </div>
-          <form onSubmit={() => console.log('Submit')}>
-            <div className='flex flex-col gap-4'>
+          <FormControl
+            onSubmit={() => console.log('Submit')}
+            className='flex flex-col w-full gap-4 p-6 items-end'>
+            <div className='flex flex-col gap-6 w-full'>
               {Object.keys(reviewResult).map((key) => {
                 const result = reviewResult[key];
-                const originalResult = props.trainingReview.originalResults[key];
+                const originalResult =
+                  props.trainingReview.originalResults[key];
                 return (
-                  <FilterReviewForm
-                    key={key}
-                    originalResult={originalResult}
-                    currentResult={result}
-                    onChange={(result: FilterResultReview) => setFilterResult(key, result)}
-                  />
+                  <>
+                    <FilterReviewForm
+                      key={key}
+                      originalResult={originalResult}
+                      currentResult={result}
+                      onChange={(result: FilterResultReview) =>
+                        setFilterResult(key, result)
+                      }
+                    />
+                    {result.agreement === false ? (
+                      <CustomTextArea
+                        result={result}
+                        onChange={(result: FilterResultReview) =>
+                          setFilterResult(key, result)
+                        }
+                      />
+                    ) : null}
+                  </>
                 );
               })}
             </div>
-            <button
-              type='submit'
-              disabled={!canSend}
-              className='btn btn-primary'
-          </form>
+            <div className='flex items-end'>
+              <Button
+                variant='contained'
+                color='primary'
+                size='small'
+                disabled={!canSend}
+                type='submit'
+                fullWidth
+                className='submit-btn !max-w-48'
+                onClick={fullfillReview}>
+                Submit
+              </Button>
+            </div>
+          </FormControl>
         </Paper>
       </div>
     </div>
@@ -129,24 +155,91 @@ const FilterReviewForm = (props: {
   currentResult: FilterResultReview;
   onChange: (result: FilterResultReview) => void;
 }) => {
-  const agree = () => props.onChange({agreement: true, comment: props.currentResult.comment});
-  const disagree = () => props.onChange({agreement: false, comment: props.currentResult.comment});
-  const setComment = (comment: string) => props.onChange({agreement: props.currentResult.agreement, comment});
+  const agree = () =>
+    props.onChange({agreement: true, comment: props.currentResult.comment});
+  const disagree = () =>
+    props.onChange({agreement: false, comment: props.currentResult.comment});
+
+  const getStyles = (isAgreement, isError) => {
+    if (isAgreement) return '!text-success !border-success';
+    if (isError) return '!text-error !border-error';
+    return '!text-gray-300 !border-gray-300 opacity-40';
+  };
+
+  const {agreement} = props.currentResult;
+  const isAgreementTrue = agreement === true;
+  const isAgreementFalse = agreement === false;
+  const successSelectedStyles = getStyles(isAgreementTrue, false);
+  const errorSelectedStyles = getStyles(false, isAgreementFalse);
+
   return (
-    <div className='flex gap-8 items-center justify-end w-86 h-12 p-4'>
-      <Typography className='body'>{props.originalResult.filterDescription}</Typography>
-      <FormGroup>
+    <div className='flex w-full justify-between'>
+      <Typography className='body'>
+        {props.originalResult.filterDescription}
+      </Typography>
+      <FormGroup className='flex flex-col'>
         <FormControlLabel
-            control={<div>
-              <ToggleButton selected={props.currentResult.agreement === true} value="Agree" onClick={agree}>Agree</ToggleButton>
-              <ToggleButton selected={props.currentResult.agreement === false} value="Disagree" onClick={disagree}>Agree</ToggleButton>
-              <TextareaAutosize onChange={(event) => setComment(event.target.value)}></TextareaAutosize>
-            </div>}
-          label='Agree'
+          control={
+            <div className='flex flex-col gap-4'>
+              <div className='flex gap-4'>
+                <ToggleButton
+                  selected={props.currentResult.agreement === true}
+                  value='Agree'
+                  onClick={agree}
+                  size='small'
+                  className={successSelectedStyles}>
+                  Agree
+                </ToggleButton>
+                <ToggleButton
+                  selected={props.currentResult.agreement === false}
+                  value='Disagree'
+                  size='small'
+                  onClick={disagree}
+                  className={errorSelectedStyles}>
+                  Disagree
+                </ToggleButton>
+              </div>
+            </div>
+          }
+          label='Si'
           labelPlacement='start'
           className='gap-4'
         />
       </FormGroup>
     </div>
+  );
+};
+
+const CustomTextArea = (props: {
+  onChange: (result: FilterResultReview) => void;
+  result: FilterResultReview;
+}) => {
+  const setComment = (comment: string) =>
+    props.onChange({agreement: props.result.agreement, comment});
+  const handleChange = (event) => {
+    const comment = event.target.value;
+    setComment(comment);
+  };
+  return (
+    <Box
+      component={TextareaAutosize}
+      onChange={handleChange}
+      placeholder='Write a comment...'
+      sx={{
+        width: '100%',
+        backgroundColor: '#313131',
+        color: '#d7dadb',
+        fontSize: '12px',
+        minHeight: '100px',
+        padding: '10px',
+        '&:focus': {
+          borderColor: '#707070',
+          outline: 'none',
+          boxShadow: '0 0 0 1px rgba(255, 255, 255, 0.25)',
+        },
+        borderRadius: '6px',
+        border: '1px solid #707070',
+      }}
+    />
   );
 };
