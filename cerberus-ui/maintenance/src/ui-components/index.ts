b9b5cc1f8fdcdb288737e442ae151e05bdc59bc8
Ignasi Reixach <ignasireixach@gmail.com>
2024-07-08 16:20:58 +0200

    feat: no data for tables

--- cerberus-ui/maintenance/src/ui-components/index.ts
+++ cerberus-ui/maintenance/src/ui-components/index.ts
@@ -1,6 +1,7 @@
 export * from './issues-table/component';
 export * from './list-open-issues-key-performance-indicators/component';
 export * from './list-training-key-performance-indicators/component';
+export * from './no-data/component';
 export * from './open-issues-key-performance-indicators-item/component';
 export * from './report-chart/component';
 export * from './training-review-key-performance-indicators-item copy/component';
