c58e2f332a008176f69b3d3b5adc1ca6aa30edb6
angeluxee <devluxe04@gmail.com>
2025-02-18 11:13:56 +0100

    feat: implement CreateOperationHandler for operation creation

--- cerberus-ui/surveillance/src/features/operation/create/component.tsx
+++ cerberus-ui/surveillance/src/features/operation/create/component.tsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 import { InputField, Select } from "@cerberus/core";
 import { Button, Typography } from "@mui/material";
 import { OperationQuestion } from './command';
-
+import { Mediator } from 'mediatr-ts';
+import { CreateOperation } from './command';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { notificationService } from '@cerberus/core';
 type QuestionType = "Opciones" | "Númerico" | "Texto Libre";
 type ResponseSubtype = "Única" | "Múltiple";
 type Required = "Sí" | "No";
 
 export const SurveillanceOperationEditor = () => {
-    const [operationName, setOperationName] = useState("");
+    const [description, setDescription] = useState("");
     const [questions, setQuestions] = useState<OperationQuestion[]>([
         {
             id: Math.random(),
@@ -20,12 +23,43 @@ export const SurveillanceOperationEditor = () => {
         },
     ]);
 
-    const handleSelectType = (option: string) => {
-        console.log(`Selected: ${option}`);
+    const handleSubmit = async () => {
+        try {
+            console.log('description', description, 'questions', questions);
+            const operation = await new Mediator().send(new CreateOperation(description, questions));
+            notificationService.notifySuccess('successMessage');
+        } catch (e) {
+            notificationService.notifyError('errorMessage', e.message);
+            console.error(e.message);
+        }
+
     };
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value);
+    const handleSelectType = (questionIndex: number, field: string, value: string) => {
+        const updatedQuestions = [...questions];
+        updatedQuestions[questionIndex][field] = value;
+        setQuestions(updatedQuestions);
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, questionIndex?: number, field?: string, answerIndex?: number) => {
+        const { value } = e.target;
+
+        setQuestions(prevQuestions => {
+            const updatedQuestions = [...prevQuestions];
+
+            if (questionIndex !== undefined) {
+                if (field === 'title') {
+                    updatedQuestions[questionIndex].title = value;
+                } else if (answerIndex !== undefined) {
+                    updatedQuestions[questionIndex].answers[answerIndex] = value;
+                }
+            } else {
+                setDescription(value);
+                return prevQuestions;
+            }
+
+            return updatedQuestions;
+        });
     };
 
     const handleAddQuestion = () => {
@@ -47,10 +81,14 @@ export const SurveillanceOperationEditor = () => {
         updatedQuestions[questionIndex].answers = [...updatedQuestions[questionIndex].answers, ""];
         setQuestions(updatedQuestions);
     };
-
-    const handleAnswerChange = (questionIndex: number, answerIndex: number, value: string) => {
+    const handleRemoveAnswer = (questionIndex: number, answerIndex: number) => {
         const updatedQuestions = [...questions];
-        updatedQuestions[questionIndex].answers[answerIndex] = value;
+        updatedQuestions[questionIndex].answers.splice(answerIndex, 1);
+        setQuestions(updatedQuestions);
+    };
+    const handleRemoveQuestion = (questionIndex: number) => {
+        const updatedQuestions = [...questions];
+        updatedQuestions.splice(questionIndex, 1);
         setQuestions(updatedQuestions);
     };
 
@@ -60,16 +98,19 @@ export const SurveillanceOperationEditor = () => {
                 <h1 className="font-bold text-primary">Creación de Operativa - </h1>
                 <InputField
                     title=""
-                    onChange={handleInputChange}
+                    onChange={(e) => handleInputChange(e)}
                     placeholder="Añade el nombre de tu nueva operativa"
                 />
             </div>
             {questions.map((question, index) => (
                 <div className="flex flex-col gap-2 bg-tableBg py-4 px-6 rounded-[10px] w-full" key={question.id}>
-                    <h1 className="font-bold">Pregunta {index + 1}: </h1>
+                    <div className="flex gap-4 justify-between items-center">
+                        <h1 className="font-bold">Pregunta {index + 1}: </h1>
+                        <button onClick={() => handleRemoveQuestion(index)} className='flex bg-red-500 p-1 rounded-full hover:bg-red-300 '><DeleteOutlineIcon /></button>
+                    </div>
                     <InputField
                         title=""
-                        onChange={handleInputChange}
+                        onChange={(e) => handleInputChange(e, index, 'title')}
                         placeholder="Añade título a tu pregunta"
                     />
                     <div className="flex gap-4 mt-2">
@@ -77,28 +118,37 @@ export const SurveillanceOperationEditor = () => {
                         <Select
                             title="Tipología de respuesta"
                             options={['Opciones', 'Númerico', 'Texto Libre']}
-                            onSelect={handleSelectType}
+                            onSelect={(value) => handleSelectType(index, "responseType", value)}
                         />
                         <Select
                             title="Subtipología de respuesta"
                             options={['Única', 'Múltiple']}
-                            onSelect={handleSelectType}
+                            onSelect={(value) => handleSelectType(index, "responseSubtype", value)}
                         />
                         <Select
                             title="Obligatoriedad"
                             options={['Sí', 'No']}
-                            onSelect={handleSelectType}
+                            onSelect={(value) => handleSelectType(index, "required", value)}
                         />
                     </div>
                     {question.answers.map((answer, answerIndex) => (
                         <div key={answerIndex}>
                             <h1 className="font-bold mt-2">Respuesta {answerIndex + 1}</h1>
-                            <InputField
-                                title=""
-                                onChange={(e) => handleAnswerChange(index, answerIndex, e.target.value)}
-                                placeholder="..."
-                                value={answer}
-                            />
+                            <div className="flex gap-4 mt-2 items-center">
+                                <InputField
+                                    title=""
+                                    onChange={(e) => handleInputChange(e, index, 'answer', answerIndex)}
+                                    placeholder="..."
+                                    value={answer}
+                                />
+                                <button
+                                    type="button"
+                                    className="text-xs uppercase bg-formSelect text-black font-bold py-2 px-8 rounded-full hover:bg-formSelectHover"
+                                    onClick={() => handleRemoveAnswer(index, answerIndex)}
+                                >
+                                    Eliminar
+                                </button>
+                            </div>
                         </div>
                     ))}
                     <div className="items-start mt-2">
@@ -129,6 +179,7 @@ export const SurveillanceOperationEditor = () => {
             </div>
             <div className="flex">
                 <button
+                    onClick={handleSubmit}
                     type="button"
                     className="text-xs uppercase bg-secondary text-white font-bold py-2 px-8 rounded-full ml-auto hover:bg-secondaryHover">
                     Proceder
