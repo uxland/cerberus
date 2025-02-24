ac932b357fc9ea01b70174692cf68a332637facc
angeluxee <devluxe04@gmail.com>
2025-02-24 10:32:57 +0100

    WIP

--- cerberus-ui/core/src/presentation/components/index.ts
+++ cerberus-ui/core/src/presentation/components/index.ts
@@ -5,4 +5,3 @@ export * from "./InputField/InputField";
 export * from "./InputField/FormInputField.tsx";
 export * from "./Slider/Slider";
 export * from "./Select/Select";
-export * from "./Select/Select-checkmarks";
