15e19ea064c09ae08eca2d76d8e7ccf379c66490
Daní Pinart <dpinart@uxland.cat>
2025-06-17 10:58:15 +0200

    chore: fixes

--- cerberus-ui/app/vite.config.ts
+++ cerberus-ui/app/vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 5173,
-    https:{
-      key: fs.readFileSync(path.resolve(__dirname, 'certs', 'key.pem')),
-        cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'cert.pem'))
-    }
+    port: 8080,
   }
 });
