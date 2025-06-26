986e47d35860cac816520ce5209e67159adc6216
angeluxee <devluxe04@gmail.com>
2025-06-26 17:49:55 +0200

    Refactor ApiClient to use axiosInstance for consistent API requests and improve error handling in handleResponse method.

--- cerberus-ui/core/src/api/api-client.ts
+++ cerberus-ui/core/src/api/api-client.ts
@@ -1,5 +1,5 @@
-import {ApiClient} from "@cerberus/shared/src";
-import {Axios, AxiosResponse} from 'axios';
+import { ApiClient } from "@cerberus/shared/src";
+import axios, { AxiosResponse } from 'axios';
 import { keycloak } from "../auth";
 
 // @ts-ignore
@@ -11,12 +11,12 @@ const baseConfig = {
     },
 }
 
-const axios = new Axios(baseConfig);
+const axiosInstance = axios.create(baseConfig);
 const getPayload = (requestInit: RequestInit) => requestInit.body ? JSON.stringify(requestInit.body) : undefined;
 
-export class ApiClientImpl extends ApiClient{
+export class ApiClientImpl extends ApiClient {
     async getFile(url: string, requestInit?: RequestInit | undefined): Promise<{ filename: string | undefined; content: Blob; }> {
-        const response = await axios.get(url, {
+        const response = await axiosInstance.get(url, {
             headers: this.getHeaders(requestInit),
             responseType: 'blob',
             transformResponse: []
@@ -31,54 +31,57 @@ export class ApiClientImpl extends ApiClient{
             }
         }
 
-        return {filename, content: response.data};
+        return { filename, content: response.data };
     }
 
 
     get<T>(url: string, requestInit: RequestInit | undefined): Promise<T> {
-        const request = axios.get(url, {
-                headers: this.getHeaders(requestInit),
-                responseType: 'json'
-            }
+        const request = axiosInstance.get(url, {
+            headers: this.getHeaders(requestInit),
+            responseType: 'json'
+        }
         );
         return this.handleResponse(request)
     }
 
     post<T>(url: string, requestInit: RequestInit): Promise<T> {
-        const request  = axios.post(url, getPayload(requestInit), {headers: this.getHeaders(requestInit)});
+        const request = axiosInstance.post(url, getPayload(requestInit), { headers: this.getHeaders(requestInit) });
         return this.handleResponse(request)
     }
     postFile<T>(url: string, requestInit: RequestInit): Promise<T> {
-        const request = axios.post(url, requestInit.body, {headers: this.getHeaders(requestInit)});
+        const request = axiosInstance.post(url, requestInit.body, { headers: this.getHeaders(requestInit) });
         return this.handleResponse(request);
     }
 
     put<T>(url: string, requestInit: RequestInit): Promise<T> {
-        const request = axios.put(url, getPayload(requestInit), {headers: this.getHeaders(requestInit)});
+        const request = axiosInstance.put(url, getPayload(requestInit), { headers: this.getHeaders(requestInit) });
         return this.handleResponse(request)
     }
 
     delete<T>(url: string): Promise<T> {
-        const request = axios.delete(url);
+        const request = axiosInstance.delete(url);
         return this.handleResponse(request)
     }
 
     private getHeaders(requestInit: RequestInit | undefined): any {
-        const authHeader = {Authorization: `Bearer ${keycloak.token}`};
+        const authHeader = { Authorization: `Bearer ${keycloak.token}` };
         const requestHeaders = requestInit?.headers || {};
-        return {...baseConfig.headers, ...authHeader, ...requestHeaders};
+        return { ...baseConfig.headers, ...authHeader, ...requestHeaders };
     }
 
-    private async handleResponse<T>(request: Promise<AxiosResponse>):Promise<T>{
-        const response = await request;
-        return parseResponse(response);
+    private async handleResponse<T>(request: Promise<AxiosResponse>): Promise<T> {
+        try {
+            const response = await request;
+            return parseResponse(response);
+        } catch (error) {
+            console.log("Error en la petición:", error);
+            throw error;
+        }
     }
 }
 
 const parseResponse = (response: AxiosResponse) => {
-    if(response.status >= 400)
-        throw new Error(response.data);
-    if((response.headers['content-type'] || '').includes('application/json')){
+    if ((response.headers['content-type'] || '').includes('application/json')) {
         const result = typeof response.data === 'string' ? JSON.parse(response.data) : response.data;
         return typeof result === 'string' ? JSON.parse(result) : result;
     }
